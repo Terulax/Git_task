0cee6a8c9c2742d5dc386bd651f963b406f24198
Alex <abachelardba@wesleyan.edu>
2023-11-05 16:08:47 +0000

    You have changed the Hello World file

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,11 @@
-Cow = 'Moo'
-Duck = 'Quack'
-Pig = 'Oink'
+# Sound a Cow makes
+Cow = 'The cow goes Moo'
+
+# Sound a duck makes
+Duck = 'The duck goes Quack'
+
+# Sound a pig makes
+Pig = 'The pig goes Oink'
+
+#Sound a ghost makes
+Ghost = 'The ghost goes ooooh'
